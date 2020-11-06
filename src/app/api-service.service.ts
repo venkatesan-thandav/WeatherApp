7aef19def0d9283f2aa6594987cb5c874d88b458
venkatesan-thandav <73308885+venkatesan-thandav@users.noreply.github.com>
2020-11-06 12:37:06 +0530

    Update api-service.service.ts

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -13,14 +13,14 @@ return this.callServiceget(data);
 }
   callServicePost(data){
     this.configUrl='';
-    this.configUrl='http://api.openweathermap.org/data/2.5/weather';
+    this.configUrl='https://api.openweathermap.org/data/2.5/weather';
    return  this.http.post(this.configUrl,data,{ observe: 'response' });
 
   }
   callServiceget(loc){
     
     let apiKey: any='fb45d52f094925f13565d818416e18fe'
-    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}`);
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}`);
 
   }
 }
